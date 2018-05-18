2e6c6aecdd683da6a95d4ae2149fb03f29345f37
Rubén Illodo Brea <me@rubenillodo.com>
2018-05-18 16:49:10 +0200

    fix: Initial commit

---
+++ rule-options/import.order.js
@@ -0,0 +1,4 @@
+module.exports = {
+  groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+  'newlines-between': 'always',
+};
---
+++ rule-options/react.boolean-prop-naming.js
@@ -0,0 +1,1 @@
+module.exports = { rule: '^(is|has|are|will)[A-Z]([A-Za-z0-9]?)+' };
